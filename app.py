9b563ccdcbfcbb78aa484a7f383d7486ba86ba2e
tobah-create <tobiomotunde09@gmail.com>
2023-08-04 12:27:21 +0100

    Updated the html files and started creating models and database using sqlite.

--- app.py
+++ app.py
@@ -1,16 +1,20 @@
 from flask import Flask, render_template
-
-# Create an instance of the Flask class
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
+app.config['SQLALCHEMY_URI'] = 'sqlite://app.db'
+db = SQLAlchemy(app)
 
-from flask import Flask
-
-app = Flask(__name__)
+class Item(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(length=30), nullable=False, unique=True)
+    price = db.Column(db.Float(), nullable=False)
+    barcode = db.Column(db.String(length=12), nullable=False, unique=True)
+    description = db.Column(db.String(length=1024), nullable=False, unique=True)
 
 @app.route('/')
-# @app.route('/home')
-# def home_page():
-#     return render_template('home.html')
+@app.route('/home')
+def home_page():
+    return render_template('home.html')
 
 @app.route('/market')
 def market_page():
